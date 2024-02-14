a115b8ea44f6010c25557e902e767fb2f3e7fb4c
shogokaji <sho.kajiwara0323@gmail.com>
2024-02-14 22:46:42 +0900

    WSサーバー側の実装

--- app/server/websocket.rb
+++ app/server/websocket.rb
@@ -1,4 +1,8 @@
 require "socket"
+require "digest/sha1"
+
+# WSハンドシェイクに使用されるソルト
+WS_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
 server = TCPServer.new("localhost", 2345)
 
@@ -18,11 +22,50 @@ def get_http_request(client)
   request_header
 end
 
+def get_websocket_accept_key(request)
+  websocket_key = get_websocket_key(request)
+  return if websocket_key.nil?
+
+  Digest::SHA1.base64digest([websocket_key, WS_GUID].join)
+end
+
+def get_websocket_key(request)
+  matches = request.match(/^Sec-WebSocket-Key: (\S+)/)
+  return if matches.nil?
+
+  matches[1]
+end
+
+def response_data(websocket_accept_key)
+  [
+    "HTTP/1.1 101 Switching Protocols",
+    "Upgrade: websocket",
+    "Connection: Upgrade",
+    "Sec-WebSocket-Accept: #{websocket_accept_key}",
+    "\r\n"
+  ].join("\r\n")
+end
+
 loop do
   client = server.accept
 
+  # HTTPリクエストヘッダーを取得
   http_request = get_http_request(client)
   puts http_request
+
+  # リクエストヘッダーからSec-WebSocket-Keyを取得
+  # 取得したSec-WebSocket-KeyにGUIDをくっつけてハッシュ化&Base64エンコーディングしてSec-WebSocket-Acceptを生成
+  # クライアント側ではこのkeyを使って、WS接続を確立する
+  websocket_accept_key = get_websocket_accept_key(http_request)
+  # WS通信以外の場合はコネクションを閉じて次のリクエストを待つ
+  if websocket_accept_key.nil?
+    client.close
+    next
+  end
+
+  # レスポンスを送信
+  # ハンドシェイクのためのリクエストに対し、WSプロトコルでの通信への更新完了を返す
+  client.write(response_data(websocket_accept_key))
 
   client.close
 end
